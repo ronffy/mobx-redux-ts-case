ef8bd2e6924e0f6bf96c74b5939ff1ebda487c22
wanghairong-i <3189578410@qq.com>
2019-04-01 16:35:49 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        demo
+      </div>
+    );
+  }
+}
+
+export default App;
